87ac0ba7e1f61ce8a6c88b4dcc4b621119b623fc
Jorge Niedbalski <j@calyptia.com>
2023-08-03 11:24:36 +0200

    Fix for #3.
    - Pin aws provider to 4.67.0
    - Add missing policy permissions
    
    Signed-off-by: Jorge Niedbalski <j@calyptia.com>

--- versions.tf
+++ versions.tf
@@ -4,7 +4,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72"
+      # 5.0.0 removed support for classiclink, breaking the provider. Pinning to 4.67.0 is the only known workaround for now https://github.com/terraform-aws-modules/terraform-aws-vpc/pull/941
+      version = "4.67.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
